e62da5ee61103cfed8d8ccb4bf77fd94b5dac5ab
SamuelBoyer2082251 <2082251@cstj.qc.ca>
2023-04-28 12:17:36 -0400

    Ajout essai 3D spline

--- src/composante/ComposanteInfoImport.js
+++ src/composante/ComposanteInfoImport.js
@@ -1,8 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import AnimatedPage from "./AnimatedPage";
+import Spline from "@splinetool/react-spline";
+
+
 
 const ComposanteInfoImport = () => {
+
+
   return (
     <>
       <AnimatedPage>
@@ -17,11 +22,12 @@ const ComposanteInfoImport = () => {
             </Link>
           </div>
           <div className="img-accueil">
-            <img
+            {/* <img
               src="https://cdn.pixabay.com/photo/2022/10/17/00/02/dual-7526360_960_720.jpg"
               alt="img"
-            />
+            /> */}
           </div>
+            <Spline scene="https://prod.spline.design/P8dlKhjbHujM5uup/scene.splinecode" />
         </div>
       </AnimatedPage>
     </>
